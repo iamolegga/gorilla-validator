a65c8dc93502da2029f51d6dc5a100f0ed198b0b
iamolegga <iamolegga@gmail.com>
2025-03-28 23:05:17 +0200

    lib

---
+++ validator.go
@@ -0,0 +1,168 @@
+package gv
+
+import (
+	"context"
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"net/http"
+	"reflect"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/gorilla/mux"
+	"github.com/gorilla/schema"
+)
+
+// ErrorHandlerFunc is a function type that defines how validation errors are handled
+type ErrorHandlerFunc func(err error) http.HandlerFunc
+
+// defaultErrorHandler is the default implementation of error handling
+var currentErrorHandler = func(err error) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+	}
+}
+
+// ErrorHandler allows setting a custom error handler function
+func ErrorHandler(h ErrorHandlerFunc) {
+	currentErrorHandler = h
+}
+
+// sourceKey is a type for context keys used to store validated data. It is
+// used to avoid conflicts with other middleware that may use the same context
+// keys
+type sourceKey string
+
+// Source represents the source of input data for validation
+type Source string
+
+const (
+	Params Source = "Params"
+	Query  Source = "Query"
+	Form   Source = "Form"
+	JSON   Source = "JSON"
+	XML    Source = "XML"
+)
+
+var schemaDecoder = schema.NewDecoder()
+
+func Validate(schema any, src Source) mux.MiddlewareFunc {
+	return func(handler http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			schemaValue := reflect.New(reflect.TypeOf(schema)).Interface()
+
+			switch src {
+			case Params:
+				vars := mux.Vars(r)
+				varsFixed := make(map[string][]string)
+				for k, v := range vars {
+					varsFixed[k] = []string{v}
+				}
+				err := schemaDecoder.Decode(schemaValue, varsFixed)
+				if err != nil {
+					currentErrorHandler(err).ServeHTTP(w, r)
+					return
+				}
+			case Query:
+				err := schemaDecoder.Decode(schemaValue, r.URL.Query())
+				if err != nil {
+					currentErrorHandler(err).ServeHTTP(w, r)
+					return
+				}
+			case Form:
+				err := r.ParseForm()
+				if err != nil {
+					currentErrorHandler(err).ServeHTTP(w, r)
+					return
+				}
+				err = schemaDecoder.Decode(schemaValue, r.PostForm)
+				if err != nil {
+					currentErrorHandler(err).ServeHTTP(w, r)
+					return
+				}
+			case JSON:
+				var jsonData map[string]any
+				err := json.NewDecoder(r.Body).Decode(&jsonData)
+				if err != nil {
+					currentErrorHandler(err).ServeHTTP(w, r)
+					return
+				}
+				data := convertToMapStringSlice(jsonData)
+				err = schemaDecoder.Decode(schemaValue, data)
+				if err != nil {
+					currentErrorHandler(err).ServeHTTP(w, r)
+					return
+				}
+			case XML:
+				decoder := xml.NewDecoder(r.Body)
+				result := make(map[string]any)
+				var currentElement string
+
+				for {
+					tok, err := decoder.Token()
+					if err != nil {
+						break
+					}
+
+					switch token := tok.(type) {
+					case xml.StartElement:
+						currentElement = token.Name.Local
+					case xml.CharData:
+						if currentElement != "" {
+							result[currentElement] = string(token)
+							currentElement = ""
+						}
+					}
+				}
+				if err := schemaDecoder.Decode(schemaValue, convertToMapStringSlice(result)); err != nil {
+					currentErrorHandler(err).ServeHTTP(w, r)
+					return
+				}
+			default:
+				panic("unknown source: " + src)
+			}
+
+			validate := validator.New()
+			err := validate.Struct(schemaValue)
+			if err != nil {
+				currentErrorHandler(err).ServeHTTP(w, r)
+				return
+			}
+
+			r = r.WithContext(context.WithValue(r.Context(), sourceKey(src), schemaValue))
+			handler.ServeHTTP(w, r)
+		})
+	}
+}
+
+func Validated[T any](r *http.Request, src Source) T {
+	return r.Context().Value(sourceKey(src)).(T)
+}
+
+func convertToMapStringSlice(input map[string]any) map[string][]string {
+	result := make(map[string][]string)
+	for key, value := range input {
+		switch v := value.(type) {
+		case string:
+			result[key] = []string{v}
+		case float64:
+			result[key] = []string{fmt.Sprintf("%v", v)}
+		case bool:
+			result[key] = []string{fmt.Sprintf("%v", v)}
+		case []any:
+			var strSlice []string
+			for _, elem := range v {
+				strSlice = append(strSlice, fmt.Sprintf("%v", elem))
+			}
+			result[key] = strSlice
+		default:
+			result[key] = []string{fmt.Sprintf("%v", v)}
+		}
+	}
+	return result
+}
+
+type Element struct {
+	XMLName xml.Name
+	Value   string `xml:",chardata"`
+}
---
+++ validator_test.go
@@ -0,0 +1,226 @@
+package gv_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
+	"testing"
+
+	"github.com/gorilla/mux"
+	gv "github.com/iamolegga/gorilla-validator"
+	"github.com/stretchr/testify/assert"
+)
+
+type TestSchema struct {
+	ID    int    `schema:"id" validate:"required"`
+	Name  string `schema:"name" validate:"required"`
+	Email string `schema:"email" validate:"required,email"`
+}
+
+type ParamsTestSchema struct {
+	ID int `schema:"id" validate:"required"`
+}
+
+func TestValidateParamsOK(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/test/123", nil)
+	req = mux.SetURLVars(req, map[string]string{"id": "123"})
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(ParamsTestSchema{}, gv.Params)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data := gv.Validated[*ParamsTestSchema](r, gv.Params)
+		assert.Equal(t, 123, data.ID)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestValidateParamsError(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/test/abc", nil)
+	req = mux.SetURLVars(req, map[string]string{"id": "abc"})
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(ParamsTestSchema{}, gv.Params)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Fail(t, "should not reach here")
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestValidateQueryOK(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/test?id=123&name=John&email=john@example.com", nil)
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(TestSchema{}, gv.Query)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data := gv.Validated[*TestSchema](r, gv.Query)
+		assert.Equal(t, 123, data.ID)
+		assert.Equal(t, "John", data.Name)
+		assert.Equal(t, "john@example.com", data.Email)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestValidateQueryError(t *testing.T) {
+	req := httptest.NewRequest(http.MethodGet, "/test?id=abc&name=John&email=john@example.com", nil)
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(TestSchema{}, gv.Query)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Fail(t, "should not reach here")
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestValidateFormOK(t *testing.T) {
+	form := url.Values{}
+	form.Add("id", "123")
+	form.Add("name", "John")
+	form.Add("email", "john@example.com")
+	req := httptest.NewRequest(http.MethodPost, "/test", strings.NewReader(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(TestSchema{}, gv.Form)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data := gv.Validated[*TestSchema](r, gv.Form)
+		assert.Equal(t, 123, data.ID)
+		assert.Equal(t, "John", data.Name)
+		assert.Equal(t, "john@example.com", data.Email)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestValidateFormError(t *testing.T) {
+	form := url.Values{}
+	form.Add("id", "abc")
+	form.Add("name", "John")
+	form.Add("email", "john@example.com")
+	req := httptest.NewRequest(http.MethodPost, "/test", strings.NewReader(form.Encode()))
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(TestSchema{}, gv.Form)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Fail(t, "should not reach here")
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestValidateJSON(t *testing.T) {
+	body := `{"id":123,"name":"John","email":"john@example.com"}`
+	req := httptest.NewRequest(http.MethodPost, "/test", strings.NewReader(body))
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(TestSchema{}, gv.JSON)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data := gv.Validated[*TestSchema](r, gv.JSON)
+		assert.Equal(t, 123, data.ID)
+		assert.Equal(t, "John", data.Name)
+		assert.Equal(t, "john@example.com", data.Email)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestValidateJSONError(t *testing.T) {
+	body := `{"id":"abc","name":"John","email":"john@example.com"}`
+	req := httptest.NewRequest(http.MethodPost, "/test", strings.NewReader(body))
+	req.Header.Add("Content-Type", "application/json")
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(TestSchema{}, gv.JSON)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Fail(t, "should not reach here")
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestValidateXMLOK(t *testing.T) {
+	body := `<TestSchema><id>123</id><name>John</name><email>john@example.com</email></TestSchema>`
+	req := httptest.NewRequest(http.MethodPost, "/test", strings.NewReader(body))
+	req.Header.Add("Content-Type", "application/xml")
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(TestSchema{}, gv.XML)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		data := gv.Validated[*TestSchema](r, gv.XML)
+		assert.Equal(t, 123, data.ID)
+		assert.Equal(t, "John", data.Name)
+		assert.Equal(t, "john@example.com", data.Email)
+		w.WriteHeader(http.StatusOK)
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusOK, rr.Code)
+}
+
+func TestValidateXMLError(t *testing.T) {
+	body := `<TestSchema><id>abc</id><name>John</name><email>john@example.com</email></TestSchema>`
+	req := httptest.NewRequest(http.MethodPost, "/test", strings.NewReader(body))
+	req.Header.Add("Content-Type", "application/xml")
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(TestSchema{}, gv.XML)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Fail(t, "should not reach here")
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+}
+
+func TestErrorHandler(t *testing.T) {
+	var called bool
+
+	gv.ErrorHandler(func(err error) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			called = true
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		}
+	})
+
+	req := httptest.NewRequest(http.MethodGet, "/test/abc", nil)
+	req = mux.SetURLVars(req, map[string]string{"id": "abc"})
+
+	rr := httptest.NewRecorder()
+
+	handler := gv.Validate(ParamsTestSchema{}, gv.Params)(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Fail(t, "should not reach here")
+	}))
+
+	handler.ServeHTTP(rr, req)
+
+	assert.Equal(t, http.StatusBadRequest, rr.Code)
+	assert.True(t, called)
+}
